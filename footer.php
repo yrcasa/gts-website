b54db919a7f86a7e278cebbb9d1a7e134933e4fe
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-16 16:13:51 +0800

    minor changes

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
             <h3 class="col mb-3">About Us</h3>
             <ul class="nav-flex-column list-unstyled">
                 <li class="nav-item mb-0"><a href="about.php" class="text-decoration-none">Our Story</a></li>
-                <li class="nav-item mb-0">Careers</li>
+                <li class="nav-item mb-0"><a href="http://globaltigersolutions.com/careers.php">Careers</a></li>
                 <li class="nav-item mb-0">Resources</li>
             </ul>
         </div>
@@ -25,7 +25,7 @@
         <div class="">
             <h3>Our Solutions</h3>
             <ul class="nav-flex-column list-unstyled">
-                <li class="nav-item mb-0">Endpoint Protection and CyberSecurity</li>
+                <li class="nav-item mb-0"><a href="https://globaltigersolutions.com/partner.php">Endpoint Protection and CyberSecurity</a> </li>
                 <li class="nav-item mb-0">Network Security and Firewall</li>
                 <li class="nav-item mb-0">Privileged Access Management</li>
                 <li class="nav-item mb-0">Remote Access and Monitoring</li>
@@ -37,9 +37,9 @@
         <div class="">
             <h3>Contact Us</h3>
             <ul class="nav-flex-column list-unstyled">
-                <li class="list-item mb-0"><i class="bi bi-geo-alt" style="font-size: 10px;"></i> 3rd Floor Pet Plans Tower, 444 EDSA Guadaupe Viejo, Makati City, Philippines 1211</li>
-                <li class="list-item mb-0"><i class="bi bi-telephone" style="font-size: 10px;"></i> +63 2 8352 8250</li>
-                <li class="list-item mb-0"><i class="bi bi-envelope" style="font-size: 10px;"></i> sales@globaltigersolutions.com</li>
+                <li class="list-item mb-0"><i class="bi bi-geo-alt" style="font-size: 10px;"></i><a href="https://www.google.com/maps/place/Global+Tiger+Solutions/@14.562128,121.0405898,17z/data=!3m1!4b1!4m6!3m5!1s0x3397c91b100c8323:0xf9f0f08bfc5634bf!8m2!3d14.562128!4d121.0427785!16s%2Fg%2F11qpx18n2l"> 3rd Floor Pet Plans Tower, 444 EDSA Guadaupe Viejo, Makati City, Philippines 1211</a></li>
+                <li class="list-item mb-0"><i class="bi bi-telephone" style="font-size: 10px;"></i><a href="tel:+63 2 8352 825"> +63 2 8352 8250</a></li>
+                <li class="list-item mb-0"><i class="bi bi-envelope" style="font-size: 10px;"></i><a href="mailto:sales@globaltigersolutions.com"> sales@globaltigersolutions.com</a></li>
             </ul>
             <ul class="nav-flex-column list-unstyled list-group list-group-horizontal d-flex justify-content-center">
                 <li class="nav-item mb-0 m-1 mt-1"><a href="https://www.facebook.com/tcsdi/"><i class="bi bi-facebook" style="font-size:20px"></i></a></li>
