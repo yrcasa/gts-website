a2afb2b8ae6ffa9de6a465d04f9b3d3174c55782
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-16 14:06:13 +0800

    fix margin and columns

--- footer.php
+++ footer.php
@@ -1,55 +1,53 @@
 <footer id="footer">
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-1 my-2">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 py-1 my-2 mx-5 px-5">
         <div class="">
             <h3 class="col mb-3">About Us</h3>
             <ul class="nav-flex-column list-unstyled">
-                <li class="nav-item mb-2"><a href="about.php" class="text-decoration-none">Our Story</a></li>
-                <li class="nav-item mb-2">Careers</li>
-                <li class="nav-item mb-2">Resources</li>
+                <li class="nav-item mb-0"><a href="about.php" class="text-decoration-none">Our Story</a></li>
+                <li class="nav-item mb-0">Careers</li>
+                <li class="nav-item mb-0">Resources</li>
             </ul>
         </div>
 
-        <div class="container">
+        <div class="">
             <h3>Our Products</h3>
-            <ul class="nav-flex-column list-unstyled row d-flex justify-content-center">
-                <li class="nav-item mb-2 col-sm-5">Agisoft</li>
-                <li class="nav-item mb-2 col-sm-5">Anydesk</li>
-                <li class="nav-item mb-2 col-sm-5">Bullguard</li>
-                <li class="nav-item mb-2 col-sm-5">Foxit</li>
-                <li class="nav-item mb-2 col-sm-5">Heimdal Security</li>
-                <li class="nav-item mb-2 col-sm-5">Navicat</li>
-                <li class="nav-item mb-2 col-sm-5">Netgate</li>
-                <li class="nav-item mb-2 col-sm-5">Watchguard Security</li>
+            <ul class="nav-flex-column list-unstyled r">
+                <li class="nav-item mb-0">Agisoft</li>
+                <li class="nav-item mb-0">Anydesk</li>
+                <li class="nav-item mb-0">Bullguard</li>
+                <li class="nav-item mb-0">Foxit</li>
+                <li class="nav-item mb-0">Heimdal Security</li>
+                <li class="nav-item mb-0">Navicat</li>
+                <li class="nav-item mb-0">Netgate</li>
+                <li class="nav-item mb-0">Watchguard</li>
             </ul>
         </div>
 
         <div class="">
             <h3>Our Solutions</h3>
-            <ul class="nav-flex-column list-unstyled row d-flex justify-content-center">
-                <li class="nav-item mb-2 col-sm-5">Endpoint Protection and CyberSecurity</li>
-                <li class="nav-item mb-2 col-sm-5">Network Security and Firewall</li>
-                <li class="nav-item mb-2 col-sm-5">Privileged Access Management</li>
-                <li class="nav-item mb-2 col-sm-5">Remote Access and Monitoring</li>
-                <li class="nav-item mb-2 col-sm-5">Flight Information Display System</li>
-                <li class="nav-item mb-2 col-sm-5">PDF Editor Software & PDF Solutions</li>
-            </ul>
-        </div>
-
-        <div class="">
-            <h3>Our Office</h3>
             <ul class="nav-flex-column list-unstyled">
-                <li class="list-item mb-2 mx-5">Unit 3A Pet Plans Tower, 444 EDSA Guadaupe Viejo, Makati City</li>
+                <li class="nav-item mb-0">Endpoint Protection and CyberSecurity</li>
+                <li class="nav-item mb-0">Network Security and Firewall</li>
+                <li class="nav-item mb-0">Privileged Access Management</li>
+                <li class="nav-item mb-0">Remote Access and Monitoring</li>
+                <li class="nav-item mb-0">Flight Information Display System</li>
+                <li class="nav-item mb-0">PDF Editor Software & PDF Solutions</li>
             </ul>
         </div>
 
         <div class="">
-            <h3>Follow Us</h3>
+            <h3>Contact Us</h3>
+            <ul class="nav-flex-column list-unstyled">
+                <li class="list-item mb-0"><i class="bi bi-geo-alt" style="font-size: 10px;"></i> 3rd Floor Pet Plans Tower, 444 EDSA Guadaupe Viejo, Makati City, Philippines 1211</li>
+                <li class="list-item mb-0"><i class="bi bi-telephone" style="font-size: 10px;"></i> +63 2 8352 8250</li>
+                <li class="list-item mb-0"><i class="bi bi-envelope" style="font-size: 10px;"></i> sales@globaltigersolutions.com</li>
+            </ul>
             <ul class="nav-flex-column list-unstyled list-group list-group-horizontal d-flex justify-content-center">
-                <li class="nav-item mb-2 m-1"><a href="https://www.facebook.com/tcsdi/"><i class="bi bi-facebook" style="font-size:20px"></i></a></li>
-                <li class="nav-item mb-2 m-1"><a href="http://www.linkedin.com/company/global-tiger-solutions/"><i class="bi bi-linkedin" style="font-size: 20px;"></i></a></li>
+                <li class="nav-item mb-0 m-1 mt-1"><a href="https://www.facebook.com/tcsdi/"><i class="bi bi-facebook" style="font-size:20px"></i></a></li>
+                <li class="nav-item mb-0 m-1 mt-1"><a href="http://www.linkedin.com/company/global-tiger-solutions/"><i class="bi bi-linkedin" style="font-size: 20px;"></i></a></li>
             </ul>
         </div>
-        
+
     </div>
 
 
