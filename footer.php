0992b4ddc0636970b6f5f52a425ae659d9f52d96
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-16 11:51:40 +0800

    fix columns

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id="footer">
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 py-1 my-2">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-1 my-2">
         <div class="">
             <h3 class="col mb-3">About Us</h3>
             <ul class="nav-flex-column list-unstyled">
@@ -10,7 +10,7 @@
         </div>
 
         <div class="container">
-            <h3>Products</h3>
+            <h3>Our Products</h3>
             <ul class="nav-flex-column list-unstyled row d-flex justify-content-center">
                 <li class="nav-item mb-2 col-sm-5">Agisoft</li>
                 <li class="nav-item mb-2 col-sm-5">Anydesk</li>
@@ -22,12 +22,26 @@
                 <li class="nav-item mb-2 col-sm-5">Watchguard Security</li>
             </ul>
         </div>
+
+        <div class="">
+            <h3>Our Solutions</h3>
+            <ul class="nav-flex-column list-unstyled row d-flex justify-content-center">
+                <li class="nav-item mb-2 col-sm-5">Endpoint Protection and CyberSecurity</li>
+                <li class="nav-item mb-2 col-sm-5">Network Security and Firewall</li>
+                <li class="nav-item mb-2 col-sm-5">Privileged Access Management</li>
+                <li class="nav-item mb-2 col-sm-5">Remote Access and Monitoring</li>
+                <li class="nav-item mb-2 col-sm-5">Flight Information Display System</li>
+                <li class="nav-item mb-2 col-sm-5">PDF Editor Software & PDF Solutions</li>
+            </ul>
+        </div>
+
         <div class="">
             <h3>Our Office</h3>
             <ul class="nav-flex-column list-unstyled">
                 <li class="list-item mb-2 mx-5">Unit 3A Pet Plans Tower, 444 EDSA Guadaupe Viejo, Makati City</li>
             </ul>
         </div>
+
         <div class="">
             <h3>Follow Us</h3>
             <ul class="nav-flex-column list-unstyled list-group list-group-horizontal d-flex justify-content-center">
@@ -35,6 +49,7 @@
                 <li class="nav-item mb-2 m-1"><a href="http://www.linkedin.com/company/global-tiger-solutions/"><i class="bi bi-linkedin" style="font-size: 20px;"></i></a></li>
             </ul>
         </div>
+        
     </div>
 
 
