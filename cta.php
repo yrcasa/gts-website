1556796c8ab2e50be1a2ddd7d415cf4198b62940
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-18 16:58:29 +0800

    recreate design

--- cta.php
+++ cta.php
@@ -1,43 +1,132 @@
-<section id="cta" class="cta">
-
-      <div class="container">
-
-
-
-        <div class="row" data-aos="zoom-out">
-
-          <div class="col-lg-9 text-center text-lg-left">
-
-            <h3>OUR SOLUTIONS</h3>
-
-            <ul>
-
-              <li><a href="https://globaltigersolutions.com/partner.php">Endpoint Protection and CyberSecurity</a></li>
-
-              <li><a href="https://globaltigersolutions.com/partner.php#firewall">Network Security and Firewall</a></li>
-
-              <li><a href="#!">Privileged Access Management</a></li>
-
-              <li><a href="#!">Remote Access and Monitoring</a></li>
-
-              <li><a href="https://globaltigersolutions.com/products/enterprise/zeus.php">Flight Information Display System</a></li>
-
-              <li><a href="https://globaltigersolutions.com/partners/software/foxit.php">PDF Editor Software & PDF Solutions</a></li>
-
-            </ul>
-
-          </div>
-
-          <div class="col-lg-3 cta-btn-container text-center">
-
-            <a class="cta-btn align-middle" href="partner.php">Learn More</a>
-
-          </div>
-
-        </div>
-
-
-
-      </div>
-
-  </section>
+<section id="cta" class="cta">
+
+
+
+  <div class="container">
+
+
+
+
+
+
+
+    <div class="row row-cols-1" data-aos="zoom-out">
+
+
+
+      <div class="text-center text-lg-center flex">
+        <h3>OUR SOLUTIONS</h3>
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 py-2 my-2 mx-3">
+          <div class="col-sm-12 d-flex justify-content-center mb-3">
+            <div class="card" style="width: 350px;">
+              <img src="assets/img/higher_062519_security.png.jpg" class="card-img-top" style="height: 200px;" alt="...">
+              <div class="card-body">
+                <h4 class="card-title text-muted">Endpoint Protection and Security</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm d-flex justify-content-center mb-3">
+            <div class="card" style="width: 350px;">
+              <img src="assets/img/cyber-security-g28ada1b6f_1920.jpg" class="card-img-top" style="height: 200px; background-size: cover;" alt="...">
+              <div class="card-body">
+                <h4 class="card-title text-muted">Network Security and Firewall</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm d-flex justify-content-center mb-3">
+            <div class="card" style="width: 350px;">
+              <img src="assets/img/carousel/heimdal1.png" class="card-img-top" style="height: 200px;" alt="...">
+              <div class="card-body">
+                <h4 class="card-title text-muted">Privileged Access Management</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm d-flex justify-content-center mb-3">
+            <div class="card" style="width: 350px;">
+              <img src="assets/img/internet-gdd7b6f87d_1920.jpg" class="card-img-top" style="height: 200px;" alt="...">
+              <div class="card-body">
+                <h4 class="card-title text-muted">Remote Access and Monitoring</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm d-flex justify-content-center mb-3">
+            <div class="card" style="width: 350px;">
+              <img src="assets/img/benjamin-wong-YHlJceX8jMs-unsplash.jpg" class="card-img-top" style="height: 200px;" alt="...">
+              <div class="card-body">
+                <h4 class="card-title text-muted">Flight Information Display System</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm d-flex justify-content-center mb-3">
+            <div class="card" style="width: 350px;">
+              <img src="assets/img/How-to-Edit-a-PDF-4.png" class="card-img-top" style="height: 200px;" alt="...">
+              <div class="card-body">
+                <h4 class="card-title text-muted">PDF Editor Software & PDF Solutions</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-3 row-cols-md-3 py-1 my-1 mx-3"></div>
+      </div>
+
+      <!--
+            <ul>
+
+
+              
+              <li><a href="https://globaltigersolutions.com/partner.php">Endpoint Protection and CyberSecurity</a></li>
+
+
+
+              <li><a href="https://globaltigersolutions.com/partner.php#firewall">Network Security and Firewall</a></li>
+
+
+
+              <li><a href="#!">Privileged Access Management</a></li>
+
+
+
+              <li><a href="#!">Remote Access and Monitoring</a></li>
+
+
+
+              <li><a href="https://globaltigersolutions.com/products/enterprise/zeus.php">Flight Information Display System</a></li>
+
+
+
+              <li><a href="https://globaltigersolutions.com/partners/software/foxit.php">PDF Editor Software & PDF Solutions</a></li>
+
+
+
+            </ul>
+-->
+
+
+
+
+
+
+      <div class="col-lg-7 cta-btn-container text-center">
+
+
+
+        <a class="cta-btn align-middle" href="partner.php">Learn More</a>
+
+
+
+      </div>
+
+
+    </div>
+
+
+
+
+
+
+
+  </div>
+
+
+
+</section>
