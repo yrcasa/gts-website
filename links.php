6d8ccadc32155994076d4292ff9a52f53b843c5c
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-19 16:24:40 +0800

    first commit

---
+++ links.php
@@ -0,0 +1,46 @@
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="Author" content="Global Tiger Solutions">
+    <meta name="Description" content="Global Tiger Solutions provides companies with cost-effective enterprise business solutions.">
+    <meta name="keywords" content="tcsdi, global tiger solutions, internet security, antivirus distributor, endpoint protection, flight information display, fids, foxit, anydesk, bullguard, heimdal, Cybersecurity">
+    <!-- Keywords Used: 2,3,4,5,12,13,14,17,23,24 -->
+    <meta name="robots" content="Index, Follow">
+    <meta name="revisit-after" content="7 days">
+    <!-- Favicons -->
+    <link rel="icon" href="../assets/img/tcsdi_icon.ico">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+    <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> -->
+    <!-- Template Main CSS File -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9144299-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-9144299-1');
+        /* Custom Search Element API */
+        (function() {
+            var cx = '004381368140357569196:v38j-dz2r1k';
+            var gcse = document.createElement('script');
+            gcse.type = 'text/javascript';
+            gcse.async = true;
+            gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(gcse, s);
+        })();
+    </script>
