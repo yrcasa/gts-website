f0d6c3df2495eda402f8bff4d69510dd02fae7c2
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-19 16:25:52 +0800

    replace outdated logo

--- partnersSlider.php
+++ partnersSlider.php
@@ -1,35 +1,69 @@
-<section id="partners" class="partners">
-      <div class="container">
-
-        <div class="section-title" data-aos="zoom-out">
-          <h2>some of our valued partners</h2>
-          <!-- <p>WHAT WE CAN OFFER FOR THE BETTERMENT OF YOUR COMPANY</p> -->
-        </div>
-
-        <div class="owl-carousel partners-carousel" data-aos="fade-up">
-          <div class="partners-item"><img src="assets/img/solutions/adobe.png" halign="center" > </div>
-          <div class="partners-item"><img src="assets/img/solutions/anydesk.png" halign="center" > </div>
-
-          <div class="partners-item"><img src="assets/img/solutions/apc.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/bullguard.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/eaton.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/epson.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/foxit-logo.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/heimdal.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/hpe.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/lenovo.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/microsoft.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/nagios.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/navicat_logo_rainbow.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/netgate.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/oracle.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/ontrackauthorizedpartner_c.png" halign="center"  > </div>
-
-          <div class="partners-item"><img src="assets/img/solutions/panda.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/pfslogo.png" halign="center"  > </div>
-          <div class="partners-item"><img src="assets/img/solutions/sprout-social-logo-new.png" halign="center" height="70px"  > </div>
-
-          <div class="partners-item"><img src="assets/img/solutions/watchguard.png" halign="center"  > </div>
-        </div>
-
+<section id="partners" class="partners">
+
+      <div class="container">
+
+
+
+        <div class="section-title" data-aos="zoom-out">
+
+          <h2>some of our valued partners</h2>
+
+          <!-- <p>WHAT WE CAN OFFER FOR THE BETTERMENT OF YOUR COMPANY</p> -->
+
+        </div>
+
+
+
+        <div class="owl-carousel partners-carousel" data-aos="fade-up">
+
+          <div class="partners-item"><img src="assets/img/solutions/adobe.png" halign="center" > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/anydesk.png" halign="center" > </div>
+
+
+
+          <div class="partners-item"><img src="assets/img/solutions/apc.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/bullguard.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/eaton.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/epson.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/foxit-logo.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/heimdal1.png" halign="center" height="50px"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/hpe.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/lenovo-logo-1-1.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/microsoft.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/nagios.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/navicat_logo_rainbow.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/netgate.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/oracle.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/ontrackauthorizedpartner_c.png" halign="center"  > </div>
+
+
+
+          <div class="partners-item"><img src="assets/img/solutions/panda.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/pfslogo.png" halign="center"  > </div>
+
+          <div class="partners-item"><img src="assets/img/solutions/sprout-social-logo-new.png" halign="center" height="70px"  > </div>
+
+
+
+          <div class="partners-item"><img src="assets/img/solutions/watchguard.png" halign="center"  > </div>
+
+        </div>
+
+
+
       </section>
