7faefd8e2a23ef88bc8dd1a6ecf4cde65fb586cf
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-19 16:23:44 +0800

    first commit

---
+++ solutions/networkSecurity.php
@@ -0,0 +1,107 @@
+<?php include('loader.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Network Security and Firewall | Global Tiger Solutions</title>
+    <?php include "../links.php" ?>
+    <link href="../assets/css/products-styles/partners.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+
+    <header id="header" class="fixed-top d-flex align-items-center  header-transparent header-scrolled">
+
+        <div class="container d-flex align-items-center">
+
+
+
+            <div class="logo mr-auto">
+
+                <h1 class="text-light">
+
+                    <!-- <a href="index.html">GTS</a> -->
+
+                    <a href="index.php"><img src="../assets/img/gtslogo.png" alt="" class="img-fluid"></a>
+
+                </h1>
+
+                <!-- Uncomment below if you prefer to use an image logo -->
+
+            </div>
+
+
+
+            <!-- ======= Nav Menu Section ======= -->
+
+            <?php include('../navMenu.php'); ?>
+
+
+
+        </div>
+
+    </header><!-- End Header -->
+    <main id="main">
+        <section class="partner" style="padding-top: 100px !important;">
+            <div class="container">
+                <div class="section-title" data-aos="zoom-out">
+                    <p>Solutions</p>
+                    <h2>Network Security and Firewall</h2>
+                </div>
+                <div class="row content">
+                    <div class="col-lg-4 col-md-8 col-sm-12 center-block">
+                        <img src="../assets/img/solutions/netgate.png" alt="" srcset="" width="100%">
+                    </div>
+                    <div class="col-md-8 col-sm-16 mt-3">
+                        <h1>Netgate</h1>
+                        <p>
+                            Netgate is a leading provider of open-source-based network security solutions. With a strong focus on security, reliability, and performance, Netgate offers a range of products, including pfSense firewall appliances and software. Their solutions are designed to meet the needs of businesses, government agencies, and individuals, providing robust network protection, secure VPN connectivity, and advanced threat management. Netgate's commitment to open-source technologies ensures transparency, flexibility, and community collaboration. Choose Netgate for powerful, cost-effective, and customizable network security solutions that deliver exceptional performance and peace of mind.
+                        </p>
+                    </div>
+                </div>
+                <div class="row content py-2">
+                    <div class="col-lg-4 col-md-8 col-sm-12 center-block mt-4">
+                        <img src="../assets/img/solutions/watchguard.png" alt="" srcset="" width="100%">
+                    </div>
+                    <div class="col-md-8 col-sm-16 mt-4">
+                        <h1>Watchguard</h1>
+                        <p>
+                            WatchGuard is a prominent provider of network security solutions, offering industry-leading firewalls, secure Wi-Fi, and multifactor authentication. With a focus on delivering robust protection, simplified management, and high-performance security services, WatchGuard empowers organizations to safeguard their networks and data against the evolving threat landscape. Backed by innovative technologies and a commitment to customer success, WatchGuard helps businesses of all sizes mitigate risks, maintain compliance, and achieve comprehensive network security.
+                        </p>
+                    </div>
+                </div>
+                <div class="row content px-4">
+                    <a class="btn-learn-more" href="../../partner.php#softwarePartners">
+                        <i class="icofont-arrow-left"></i>
+                        Other Partners
+                    </a>
+                </div>
+            </div>
+        </section>
+        <?php include "../subPartnersSlider.php"; ?>
+    </main>
+
+    <!-- Footer Section  -->
+    <?php include "../footer.php" ?>
+    <!--End Footer Section -->
+
+    <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+    <!-- Vendor JS Files -->
+    <script src="../../assets/vendor/jquery/jquery.min.js"></script>
+    <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="../../assets/vendor/php-email-form/validate.js"></script>
+    <script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="../../assets/vendor/venobox/venobox.min.js"></script>
+    <script src="../../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="../../assets/vendor/aos/aos.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="../../assets/js/pandasecurity-main.js"></script>
+</body>
+
+</html>
