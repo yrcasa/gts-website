a062fd06e91c374fed6358c15f44c8e69aa48afd
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-24 11:45:15 +0800

    added carousel

--- cta.php
+++ cta.php
@@ -1,178 +1,87 @@
 <section id="cta" class="cta">
-
-
-
   <div class="container">
-
-
-
-
-
-
-
     <div class="row row-cols-1" data-aos="zoom-out">
-
-
-
       <div class="text-center text-lg-center flex">
         <h3>OUR SOLUTIONS</h3>
-        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 py-2 my-2 mx-3">
-          <div class="col-sm-12 d-flex justify-content-center mb-3">
-            <div class="card" style="width: 350px;">
-              <img
-              sizes="(max-width: 1400px) 100vw, 1400px"
-              srcset="
-                assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_200.jpg 200w,
-                assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_636.jpg 636w,
-                assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1041.jpg 1041w,
-                assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1400.jpg 1400w"
-              src="assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1400.jpg" class="card-img-top" style="height: 200px;" alt="...">
-              <div class="card-body">
-                <a href="https://globaltigersolutions.com/partner.php">
-                  <h4 class="card-title text-muted">Endpoint Protection and Security</h4>
-                </a>
+        <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-interval="100">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 py-2 my-2 mx-3">
+                <div class="col d-flex justify-content-center mb-3">
+                  <div class="card" style="width: 350px;">
+                    <img sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_200.jpg 200w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_636.jpg 636w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1041.jpg 1041w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1400.jpg 1400w" src="assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1400.jpg" class="card-img-top" style="height: 200px;" alt="...">
+                    <div class="card-body">
+                      <a href="https://globaltigersolutions.com/partner.php">
+                        <h4 class="card-title text-muted">Endpoint Protection and Security</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col d-flex justify-content-center mb-3">
+                  <div class="card" style="width: 350px;">
+                    <img sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_200.jpg 200w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_559.jpg 559w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_778.jpg 778w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_960.jpg 960w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1040.jpg 1040w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1188.jpg 1188w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1257.jpg 1257w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1345.jpg 1345w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1400.jpg 1400w" src="assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1400.jpg" class="card-img-top" style="height: 200px;" alt="...">
+                    <div class="card-body">
+                      <h4 class="card-title text-muted">Network Security and Firewall</h4>
+                    </div>
+                  </div>
+                </div>
+                <div class="col d-flex justify-content-center mb-3">
+                  <div class="card" style="width: 350px;">
+                    <img src="assets/img/carousel/heimdal1.png?format=webp" class="card-img-top" style="height: 200px;" alt="...">
+                    <div class="card-body">
+                      <h4 class="card-title text-muted">Privileged Access Management</h4>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-sm d-flex justify-content-center mb-3">
-            <div class="card" style="width: 350px;">
-              <img 
-              sizes="(max-width: 1400px) 100vw, 1400px"
-              srcset="
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_200.jpg 200w,
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_559.jpg 559w,
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_778.jpg 778w,
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_960.jpg 960w,
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1040.jpg 1040w,
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1188.jpg, 1188w,
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1257.jpg, 1257w,
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1345.jpg, 1345w,
-                assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1400.jpg, 1400w"
-                src="assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1400.jpg" class="card-img-top" style="height: 200px; background-size: cover;" alt="...">
-              <div class="card-body">
-                <h4 class="card-title text-muted">Network Security and Firewall</h4>
+            <div class="carousel-item">
+              <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 py-2 my-2 mx-3">
+                <div class="col d-flex justify-content-center mb-3">
+                  <div class="card" style="width: 350px;">
+                    <img sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_200.jpg 200w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_681.jpg 681w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_962.jpg 962w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1197.jpg 1197w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1354.jpg 1354w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1400.jpg 1400w" src="assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1400.jpg" class="card-img-top" style="height: 200px;" alt="...">
+                    <div class="card-body">
+                      <h4 class="card-title text-muted">Remote Access and Monitoring</h4>
+                    </div>
+                  </div>
+                </div>
+                <div class="col d-flex justify-content-center mb-3">
+                  <div class="card" style="width: 350px;">
+                    <img sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_200.jpg 200w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_466.jpg 466w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_658.jpg 658w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_835.jpg 835w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_995.jpg 995w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1151.jpg 1151w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1248.jpg 1248w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1363.jpg 1363w" src="assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1400.jpg" class="card-img-top" style="height: 200px;" alt="...">
+                    <div class="card-body">
+                      <h4 class="card-title text-muted">Flight Information Display System</h4>
+                    </div>
+                  </div>
+                </div>
+                <div class="col d-flex justify-content-center mb-3">
+                  <div class="card" style="width: 350px;">
+                    <img sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_200.png 200w, assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_625.png 625w, assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_825.png 825w" src="assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_825.png" class="card-img-top" style="height: 200px;" alt="...">
+                    <div class="card-body">
+                      <h4 class="card-title text-muted">PDF Editor Software & PDF Solutions</h4>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-sm d-flex justify-content-center mb-3">
-            <div class="card" style="width: 350px;">
-              <img src="assets/img/carousel/heimdal1.png?format=webp" class="card-img-top" style="height: 200px;" alt="...">
-              <div class="card-body">
-                <h4 class="card-title text-muted">Privileged Access Management</h4>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm d-flex justify-content-center mb-3">
-            <div class="card" style="width: 350px;">
-              <img
-              sizes="(max-width: 1400px) 100vw, 1400px"
-              srcset="
-                assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_200.jpg 200w,
-                assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_681.jpg 681w,
-                assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_962.jpg 962w,
-                assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1197.jpg 1197w,
-                assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1354.jpg 1354w,
-                assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1400.jpg 1400w" 
-              src="assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1400.jpg" class="card-img-top" style="height: 200px;" alt="...">
-              <div class="card-body">
-                <h4 class="card-title text-muted">Remote Access and Monitoring</h4>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm d-flex justify-content-center mb-3">
-            <div class="card" style="width: 350px;">
-              <img 
-              sizes="(max-width: 1400px) 100vw, 1400px" 
-              srcset="
-                assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_200.jpg 200w, 
-                assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_466.jpg 466w, 
-                assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_658.jpg 658w, 
-                assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_835.jpg 835w,
-                assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_995.jpg 995w,
-                assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1151.jpg 1151w,
-                assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1248.jpg 1248w,
-                assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1363.jpg 1363w" 
-                src="assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1400.jpg" class="card-img-top" style="height: 200px;" alt="Flight Information">
-              <div class="card-body">
-                <h4 class="card-title text-muted">Flight Information Display System</h4>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm d-flex justify-content-center mb-3">
-            <div class="card" style="width: 350px;">
-              <img sizes="(max-width: 1400px) 100vw, 1400px"
-              srcset="
-              assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_200.png 200w,
-              assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_625.png 625w,
-              assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_825.png 825w"
-              src="assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_825.png" class="card-img-top" style="height: 200px;" alt="...">
-              <div class="card-body">
-                <h4 class="card-title text-muted">PDF Editor Software & PDF Solutions</h4>
-              </div>
-            </div>
+            <!-- Add more carousel items as needed -->
           </div>
         </div>
         <div class="row row-cols-1 row-cols-sm-3 row-cols-md-3 py-1 my-1 mx-3"></div>
       </div>
-
-      <!--
-            <ul>
-
-
-              
-              <li><a href="https://globaltigersolutions.com/partner.php">Endpoint Protection and CyberSecurity</a></li>
-
-
-
-              <li><a href="https://globaltigersolutions.com/partner.php#firewall">Network Security and Firewall</a></li>
-
-
-
-              <li><a href="#!">Privileged Access Management</a></li>
-
-
-
-              <li><a href="#!">Remote Access and Monitoring</a></li>
-
-
-
-              <li><a href="https://globaltigersolutions.com/products/enterprise/zeus.php">Flight Information Display System</a></li>
-
-
-
-              <li><a href="https://globaltigersolutions.com/partners/software/foxit.php">PDF Editor Software & PDF Solutions</a></li>
-
-
-
-            </ul>
--->
-
-
-
-
-
-
       <div class="col-lg-7 cta-btn-container text-center">
-
-
-
         <a class="cta-btn align-middle" href="partner.php">Learn More</a>
-
-
-
       </div>
-
-
     </div>
-
-
-
-
-
-
-
   </div>
-
-
-
 </section>
+
+<!-- Include Bootstrap CSS and JS files -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+
+<!-- Initialize the carousel -->
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    new bootstrap.Carousel(document.getElementById("carouselExample"));
+  });
+</script>
