4aee9a1e579fc6174ef229c60925a4f8f17f7b37
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-25 16:39:04 +0800

    beautified code

--- solutions/endpoint.php
+++ solutions/endpoint.php
@@ -3,106 +3,73 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="tcsdi endpoint, gts endpoint, global tiger solutions endpoint, endpoint security, endpoint protection, cybersecurity">
-    <title>Endpoint Protection and Security | Global Tiger Solutions</title>
-    <?php include "../links.php" ?>
-    <link href="../assets/css/products-styles/partners.css" rel="stylesheet">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="tcsdi endpoint, gts endpoint, global tiger solutions endpoint, endpoint security, endpoint protection, cybersecurity">
+	<title>Endpoint Protection and Security | Global Tiger Solutions</title> <?php include "../links.php" ?>
+	<link href="../assets/css/products-styles/partners.css" rel="stylesheet">
 </head>
 
 <body>
-    <!-- ======= Header ======= -->
-
-    <header id="header" class="fixed-top d-flex align-items-center  header-transparent header-scrolled">
-
-        <div class="container d-flex align-items-center">
-
-
-
-            <div class="logo mr-auto">
-
-                <h1 class="text-light">
-
-                    <!-- <a href="index.html">GTS</a> -->
-
-                    <a href="index.php"><img src="../assets/img/gtslogo.png" alt="" class="img-fluid"></a>
-
-                </h1>
-
-                <!-- Uncomment below if you prefer to use an image logo -->
-
-            </div>
-
-
-
-            <!-- ======= Nav Menu Section ======= -->
-
-            <?php include('../navMenu.php'); ?>
-
-
-
-        </div>
-
-    </header><!-- End Header -->
-    <main id="main">
-        <section class="partner" style="padding-top: 100px !important;">
-            <div class="container">
-                <div class="section-title" data-aos="zoom-out">
-                    <p>Solutions</p>
-                    <h2>Endpoint Protection and Security</h2>
-                </div>
-                <div class="row content">
-                    <div class="col-lg-4 col-md-8 col-sm-12 center-block">
-                        <img src="../assets/img/solutions/heimdal.png" alt="" srcset="" width="100%">
-                    </div>
-                    <div class="col-md-8 col-sm-16 mt-3">
-                        <h1>Heimdal Security</h1>
-                        <p>
-                            Heimdal Security is a leading cybersecurity company that protects individuals and businesses from online threats. With advanced solutions like Thor, we detect and block malware, ransomware, and phishing attacks in real-time. Our proactive approach includes traffic filtering and patch management to ensure secure browsing and software protection. Trust Heimdal Security for comprehensive cybersecurity services and reliable digital defense.
-                        </p>
-                    </div>
-                </div>
-                <div class="row content py-2">
-                    <div class="col-lg-4 col-md-8 col-sm-12 center-block mt-4">
-                        <img src="../assets/img/solutions/watchguard.png" alt="" srcset="" width="100%">
-                    </div>
-                    <div class="col-md-8 col-sm-16 mt-4">
-                        <h1>Watchguard</h1>
-                        <p>
-                            WatchGuard is a prominent provider of network security solutions, offering industry-leading firewalls, secure Wi-Fi, and multifactor authentication. With a focus on delivering robust protection, simplified management, and high-performance security services, WatchGuard empowers organizations to safeguard their networks and data against the evolving threat landscape. Backed by innovative technologies and a commitment to customer success, WatchGuard helps businesses of all sizes mitigate risks, maintain compliance, and achieve comprehensive network security.
-                        </p>
-                    </div>
-                </div>
-                <div class="row content px-4">
-                    <a class="btn-learn-more" href="../../partner.php#softwarePartners">
-                        <i class="icofont-arrow-left"></i>
-                        Other Partners
-                    </a>
-                </div>
-            </div>
-        </section>
-        <?php include "../subPartnersSlider.php"; ?>
-    </main>
-
-    <!-- Footer Section  -->
-    <?php include "../footer.php" ?>
-    <!--End Footer Section -->
-
-    <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
-    <!-- Vendor JS Files -->
-    <script src="../../assets/vendor/jquery/jquery.min.js"></script>
-    <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-    <script src="../../assets/vendor/php-email-form/validate.js"></script>
-    <script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="../../assets/vendor/venobox/venobox.min.js"></script>
-    <script src="../../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-    <script src="../../assets/vendor/aos/aos.js"></script>
-
-    <!-- Template Main JS File -->
-    <script src="../../assets/js/pandasecurity-main.js"></script>
+	<!-- ======= Header ======= -->
+	<header id="header" class="fixed-top d-flex align-items-center  header-transparent header-scrolled">
+		<div class="container d-flex align-items-center">
+			<div class="logo mr-auto">
+				<h1 class="text-light">
+					<!-- <a href="index.html">GTS</a> -->
+					<a href="index.php"><img src="../assets/img/gtslogo.png" alt="" class="img-fluid"></a>
+				</h1>
+				<!-- Uncomment below if you prefer to use an image logo -->
+			</div>
+			<!-- ======= Nav Menu Section ======= --> <?php include('../navMenu.php'); ?>
+		</div>
+	</header><!-- End Header -->
+	<main id="main">
+		<section class="partner" style="padding-top: 100px !important;">
+			<div class="container">
+				<div class="section-title" data-aos="zoom-out">
+					<p>Solutions</p>
+					<h2>Endpoint Protection and Security</h2>
+				</div>
+				<div class="row content">
+					<div class="col-lg-4 col-md-8 col-sm-12 center-block">
+						<img src="../assets/img/solutions/heimdal.png" alt="" srcset="" width="100%">
+					</div>
+					<div class="col-md-8 col-sm-16 mt-3">
+						<h1>Heimdal Security</h1>
+						<p> Heimdal Security is a leading cybersecurity company that protects individuals and businesses from online threats. With advanced solutions like Thor, we detect and block malware, ransomware, and phishing attacks in real-time. Our proactive approach includes traffic filtering and patch management to ensure secure browsing and software protection. Trust Heimdal Security for comprehensive cybersecurity services and reliable digital defense. </p>
+					</div>
+				</div>
+				<div class="row content py-2">
+					<div class="col-lg-4 col-md-8 col-sm-12 center-block mt-4">
+						<img src="../assets/img/solutions/watchguard.png" alt="" srcset="" width="100%">
+					</div>
+					<div class="col-md-8 col-sm-16 mt-4">
+						<h1>Watchguard</h1>
+						<p> WatchGuard is a prominent provider of network security solutions, offering industry-leading firewalls, secure Wi-Fi, and multifactor authentication. With a focus on delivering robust protection, simplified management, and high-performance security services, WatchGuard empowers organizations to safeguard their networks and data against the evolving threat landscape. Backed by innovative technologies and a commitment to customer success, WatchGuard helps businesses of all sizes mitigate risks, maintain compliance, and achieve comprehensive network security. </p>
+					</div>
+				</div>
+				<div class="row content px-4">
+					<a class="btn-learn-more" href="../../partner.php#softwarePartners">
+						<i class="icofont-arrow-left"></i> Other Partners </a>
+				</div>
+			</div>
+		</section> <?php include "../subPartnersSlider.php"; ?>
+	</main>
+	<!-- Footer Section  --> <?php include "../footer.php" ?> <!--End Footer Section -->
+	<a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+	<!-- Vendor JS Files -->
+	<script src="../../assets/vendor/jquery/jquery.min.js"></script>
+	<script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="../../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+	<script src="../../assets/vendor/php-email-form/validate.js"></script>
+	<script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+	<script src="../../assets/vendor/venobox/venobox.min.js"></script>
+	<script src="../../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+	<script src="../../assets/vendor/aos/aos.js"></script>
+	<!-- Template Main JS File -->
+	<script src="../../assets/js/pandasecurity-main.js"></script>
 </body>
 
 </html>
