de073ba10ffd9cc94169fc5794f8bb079384e5b1
yrcasa <yvanroi.casa2023@gmail.com>
2023-05-26 14:46:34 +0800

    fix responsive

--- cta.php
+++ cta.php
@@ -1,63 +1,63 @@
-<section class=cta id=cta>
-  <div class=container>
+<section class="cta wrapper" id="cta">
+  <div class="container">
     <div class="row row-cols-1" data-aos=zoom-out>
       <div class="text-center flex text-lg-center">
         <h3>OUR SOLUTIONS</h3>
-        <div class="carousel slide" data-bs-ride=carousel data-interval=10 id=carouselExample>
-          <div class=carousel-inner>
+        <div class="carousel slide" data-bs-ride="carousel" data-interval="10" id="carouselExample">
+          <div class="carousel-inner">
             <div class="carousel-item active">
               <div class="row row-cols-1 mx-3 row-cols-md-3 my-2 py-2 row-cols-sm-1">
                 <div class="col d-flex justify-content-center mb-3">
-                  <div class=card style=width:350px>
-                    <img alt=... class=card-img-top src=assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1400.jpg style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_200.jpg 200w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_636.jpg 636w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1041.jpg 1041w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1400.jpg 1400w">
-                    <div class=card-body>
+                  <div class="card">
+                    <img alt=... class="img-card" src="assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1400.jpg"  sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_200.jpg 200w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_636.jpg 636w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1041.jpg 1041w, assets\img\cta\higher_secu_dvmzco\higher_secu_dvmzco_c_scale,w_1400.jpg 1400w">
+                    <div class="card-content">
                       <a href=https://globaltigersolutions.com/partner.php>
-                        <h4 class="card-title text-muted">Endpoint Protection and Security</h4>
+                        <h4 class="card-title">Endpoint Protection and Security</h4>
                       </a>
                     </div>
                   </div>
                 </div>
                 <div class="col d-flex justify-content-center mb-3">
-                  <div class=card style=width:350px>
-                    <img alt=... class=card-img-top src=assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1400.jpg style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_200.jpg 200w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_559.jpg 559w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_778.jpg 778w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_960.jpg 960w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1040.jpg 1040w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1188.jpg 1188w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1257.jpg 1257w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1345.jpg 1345w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1400.jpg 1400w">
-                    <div class=card-body>
-                      <h4 class="card-title text-muted">Network Security and Firewall</h4>
+                  <div class="card">
+                    <img alt=... class="img-card" src="assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1400.jpg" style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_200.jpg 200w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_559.jpg 559w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_778.jpg 778w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_960.jpg 960w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1040.jpg 1040w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1188.jpg 1188w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1257.jpg 1257w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1345.jpg 1345w, assets\img\cta\cybersecu_xyd2b8\cybersecu_xyd2b8_c_scale,w_1400.jpg 1400w">
+                    <div class="card-content">
+                      <h4 class="card-title">Network Security and Firewall</h4>
                     </div>
                   </div>
                 </div>
                 <div class="col d-flex justify-content-center mb-3">
-                  <div class=card style=width:350px>
-                    <img alt=... class=card-img-top src="assets/img/carousel/heimdal1.png?format=webp" style=height:200px>
-                    <div class=card-body>
-                      <h4 class="card-title text-muted">Privileged Access Management</h4>
+                  <div class="card">
+                    <img alt=... class="img-card" src="assets/img/carousel/heimdal1.png?format=webp" style=height:200px>
+                    <div class="card-content">
+                      <h4 class="card-title">Privileged Access Management</h4>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class=carousel-item>
+            <div class="carousel-item">
               <div class="row row-cols-1 mx-3 row-cols-md-3 my-2 py-2 row-cols-sm-1">
                 <div class="col d-flex justify-content-center mb-3">
-                  <div class=card style=width:350px>
-                    <img alt=... class=card-img-top src=assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1400.jpg style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_200.jpg 200w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_681.jpg 681w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_962.jpg 962w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1197.jpg 1197w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1354.jpg 1354w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1400.jpg 1400w">
-                    <div class=card-body>
-                      <h4 class="card-title text-muted">Remote Access and Monitoring</h4>
+                  <div class="card">
+                    <img alt=... class="card-img-top" src="assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1400.jpg" style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_200.jpg 200w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_681.jpg 681w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_962.jpg 962w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1197.jpg 1197w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1354.jpg 1354w, assets\img\cta\remote_ec4g6o\remote_ec4g6o_c_scale,w_1400.jpg 1400w">
+                    <div class="card-content">
+                      <h4 class="card-title">Remote Access and Monitoring</h4>
                     </div>
                   </div>
                 </div>
                 <div class="col d-flex justify-content-center mb-3">
-                  <div class=card style=width:350px>
-                    <img alt=... class=card-img-top src=assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1400.jpg style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_200.jpg 200w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_466.jpg 466w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_658.jpg 658w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_835.jpg 835w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_995.jpg 995w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1151.jpg 1151w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1248.jpg 1248w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1363.jpg 1363w">
-                    <div class=card-body>
-                      <h4 class="card-title text-muted">Flight Information Display System</h4>
+                  <div class="card">
+                    <img alt=... class="card-img-top" src="assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1400.jpg" style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_200.jpg 200w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_466.jpg 466w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_658.jpg 658w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_835.jpg 835w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_995.jpg 995w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1151.jpg 1151w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1248.jpg 1248w, assets\img\fids_q5dpf6\fids_q5dpf6_c_scale,w_1363.jpg 1363w">
+                    <div class="card-content">
+                      <h4 class="card-title">Flight Information Display System</h4>
                     </div>
                   </div>
                 </div>
                 <div class="col d-flex justify-content-center mb-3">
-                  <div class=card style=width:350px>
-                    <img alt=... class=card-img-top src=assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_825.png style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_200.png 200w, assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_625.png 625w, assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_825.png 825w">
-                    <div class=card-body>
-                      <h4 class="card-title text-muted">PDF Editor Software & PDF Solutions</h4>
+                  <div class="card"x>
+                    <img alt=... class="card-img-top" src="assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_825.png" style=height:200px sizes="(max-width: 1400px) 100vw, 1400px" srcset="assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_200.png 200w, assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_625.png 625w, assets\img\cta\pdf_m8z58i\pdf_m8z58i_c_scale,w_825.png 825w">
+                    <div class="card-content">
+                      <h4 class="card-title">PDF Editor Software & PDF Solutions</h4>
                     </div>
                   </div>
                 </div>
@@ -75,8 +75,8 @@
   </div>
 </section>
 
-<link href=https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css rel=stylesheet>
-<script src=https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 
 <script>
   document.addEventListener("DOMContentLoaded", function() {
