44b6cf235331ead260fffc66bfe7a955bf2488ae
yrcasa <yvanroi.casa2023@gmail.com>
2023-06-07 16:32:58 +0800

    beautify

--- productHighlights.php
+++ productHighlights.php
@@ -1,33 +1,18 @@
 <section id="project" class="project">
-
     <div class="container">
-
-
-
         <div class="section-title" data-aos="zoom-out">
-
             <h2>Products</h2>
-
             <p>Our Product Highlights</p>
-
         </div>
-
-
-
         <div class="owl-carousel product-highligths-carousel" data-aos="fade-up">
-
-
-
             <!-- Product Distrubution Service  -->
-
-
             <!--
             <div class="project-item">
 
                 <img src="assets/img/solutions/panda.png" class="project-img" halign="center" alt="Panda Security">
 
                 <!-- <h3>Panda Security</h3> -->
-                <!--
+            <!--
                 <p class="text-center">
 
                     Making Your Life Easier and Safer with Panda Security
@@ -42,118 +27,49 @@
 
             </div>
             -->
-
             <div class="project-item">
                 <img src="assets/img/solutions/norton.png" alt="Norton" srcset="" class="project-img" halign="center">
-                <p class="text-center pt-2">
-                    Your ultimate cybersecurity partner
-                </p>
+                <p class="text-center pt-2"> Your ultimate cybersecurity partner </p>
                 <div style="bottom: 0;">
                     <a href="partners/endpoint/norton.php" class="btn-learn-more">Learn More</a>
                 </div>
             </div>
-
-
             <!-- Service 3  -->
-
             <div class="project-item">
-
                 <img src="assets/img/solutions/netgate.png" class="project-img" halign="center" alt="Netgate">
-
                 <!-- <h3>Netgate</h3> -->
-
-                <p class="text-center">
-
-                    Secure networking solutions. Unmatched value.
-
-                </p>
-
+                <p class="text-center"> Secure networking solutions. Unmatched value. </p>
                 <div style="bottom: 0;">
-
                     <a href="partners/firewall/netgate.php" class="btn-learn-more">Learn More</a>
-
                 </div>
-
             </div>
-
-
-
             <!-- Service 4  -->
-
             <div class="project-item">
-
                 <img src="assets/img/solutions/agisoft.png" class="project-img" halign="center" alt="Agisoft">
-
                 <!-- <h3>Agisoft</h3> -->
-
-                <p class="text-center">
-
-                    Discover intelligent photogrammetry with Metashape.
-
-                </p>
-
+                <p class="text-center"> Discover intelligent photogrammetry with Metashape. </p>
                 <div style="bottom: 0;">
-
                     <a href="partners/software/agisoft.php" class="btn-learn-more">Learn More</a>
-
                 </div>
-
             </div>
-
-
-
             <!-- Service 4  -->
-
             <div class="project-item">
-
                 <img src="assets/img/solutions/foxit-logo.webp" class="project-img" halign="center" alt="Foxit">
-
                 <!-- <h3>Foxit</h3> -->
-
-                <p class="text-center pt-2">
-
-                    Do More with Documents
-
-                </p>
-
+                <p class="text-center pt-2"> Do More with Documents </p>
                 <div style="bottom: 0;">
-
                     <a href="partners/software/foxit.php" class="btn-learn-more">Learn More</a>
-
                 </div>
-
             </div>
-
-
-
             <!-- Service 4  -->
-
             <div class="project-item">
-
                 <img src="assets/img/solutions/watchguard.png" class="project-img" halign="center" alt="WatchGuard">
-
                 <!-- <h3>WatchGuard</h3> -->
-
-                <p class="text-center">
-
-                    Smart Security, Simply Done
-
-                </p>
-
+                <p class="text-center"> Smart Security, Simply Done </p>
                 <div style="bottom: 0;">
-
                     <a href="partners/firewall/watchguard.php" class="btn-learn-more">Learn More</a>
-
                 </div>
-
             </div>
-
-
-
         </div>
-
-
-
     </div>
-
 </section>
