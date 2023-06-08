17e0b749dc6d71b7b1f1128f0beea178d98ceadb
yrcasa <yvanroi.casa2023@gmail.com>
2023-06-08 16:57:18 +0800

    update design

--- partners/remote/anydesk.php
+++ partners/remote/anydesk.php
@@ -1,257 +1,531 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <meta name="Author" content="Tiger Consulting and Software Devt Inc.">
-  <!--<meta name="robots" content="index,follow">-->
-  <meta name="Description" content="Tiger Consulting is a CERTIFIED partner and reseller of anydesk.">
-  <meta name="keywords" content="anydesk, anydesk software, tcsdi anydesk software, tiger anydesk software, tiger consulting anydesk software, anydesk certified partner, certified partner anydesk, certified partner of anydesk, authorized reseller anydesk, authorized reseller of anydesk, authorized re-seller anydesk, authorized re-seller of anydesk, adobe authorized reseller, anydesk authorized re-seller">
-  <title>Products - Anydesk | Global Tiger Solutions</title>
-
-  <!-- Favicons -->
-  <link rel="icon" href="../../assets/img/tcsdi_icon.ico">
-  <link href="../../assets/img/GTSLogo.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="../../assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="../../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/aos/aos.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="../../assets/css/products-styles/partners.css" rel="stylesheet">
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js "></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js "></script>
-<![endif]-->
-  <script>
-    (function(i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r;
-      i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date();
-      a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-64201982-1', 'auto');
-    ga('send', 'pageview');
-  </script>
-
-  <style>
-    .anydesk-intro {
-      background: linear-gradient(to right, rgba(114, 28, 106, 1) 0, rgba(204, 20, 123, 1) 100%);
-      color: white;
-    }
-
-    /* .anydesk-info {
-      padding-left: 40px; 
-    } 
-    */
-
-    /* Fonts */
-    .ph {
-      font-size: 56px;
-      line-height: 58px;
-    }
-
-    .font-italic {
-      font-style: italic
-    }
-
-    .font-extrabold {
-      font-weight: 800;
-    }
-
-    .font-underline {
-      text-decoration: underline;
-    }
-
-    .label-one {
-      padding-top: 3px;
-      font-size: 42px;
-      line-height: 46px;
-    }
-
-    .label-two {
-      font-size: 26px;
-      line-height: 32px;
-    }
-
-    .contactDetails{
-      font-size: 26px;
-      line-height: 32px;
-    }
-
-    /* Spacer */
-    .spacer-16 {
-      height: 16px;
-      width: 100%;
-    }
-
-    /* Lines */
-    ul.horizontal-lines>li {
-      padding-top: 9px !important;
-      padding-bottom: 9px !important;
-      border-bottom: solid #dedede 1px;
-    }
-
-    @media only screen and (max-width: 600px) {
-      .anydesk-info {
-        padding-left: 15px;
-        font-size: xx-small;
-      }
-
-      .contactDetails{
-        font-size: 20px;
-      line-height: 22px;
-      }
-      
-    }
-  </style>
-
-</head>
-
-<body>
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center  header-transparent header-scrolled">
-    <div class="container d-flex align-items-center">
-
-      <div class="logo mr-auto">
-        <h1 class="text-light"><a href="../../index.php"><img src="../../assets/img/gtslogo.png" alt="" class="img-fluid"></a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
-      </div>
-
-    <!-- ======= Nav Menu Section ======= -->
-    <?php include "../../navMenu.php"; ?>
-
-
-    </div>
-  </header>
-  <!-- End Header -->
-
-  <main id="main">
-
-    <section id="partner" class="partner" style="padding-top: 100px !important;">
-
-      <div class="container">
-
-        <div class="section-title" data-aos="zoom-out">
-          <p>Partners</p>
-          <h2>Anydesk</h2>
-        </div>
-
-        <!-- New Version 
-        <div class="partners-img">
-              <img src="../../assets/img/productpagebanner/anydesk/anydesk-norton.jpg" alt="BG Anvitirus" width="578px" height="370px">
-        </div>
-        -->
-
-      </div>
-
-
-    </section>
-
-
-    <section id="anydesk-discover" class="pricing text-center" style="padding-top:10px !important">
-      <div class="container">
-        <img src="../../assets/img/productpagebanner/anydesk/ad_feat_0.jpg" alt="Discover Anydesk">
-        <img src="../../assets/img/productpagebanner/anydesk/ad_feat_1.jpg" alt="Remote Support">
-        <img src="../../assets/img/productpagebanner/anydesk/ad_feat_2.jpg" alt="Work From Home">
-        <img src="../../assets/img/productpagebanner/anydesk/ad_feat_3.jpg" alt="Performance | Security">
-      </div>
-    </section>
-    
-
-    <section id="anydesk-feature" class="pricing text-center" style="padding-top:10px !important">
-
-      <div id="anydesk-feature" class="section-sub-title text-center" data-aos="zoom-out">
-        <h2>Solutions</h2>
-      </div>
-
-      <div class="container">
-        <div class="row pt-4">
-
-          <div class="col-lg-4 col-md-6">
-            <div class="box featured text-center" data-aos="zoom-in">
-              <h2>
-                LITE
-              </h2>
-              <p>ONE DEVICE</p>
-              <p>1 SESSION</p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
-            <div class="box featured text-center" data-aos="zoom-in">
-              <h2>
-                PROFESSIONAL
-              </h2>
-              <p>MULTIPLE DEVICES</p>
-              <p>1 OR MORE CONCURRENT SESSIONS</p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
-            <div class="box featured text-center" data-aos="zoom-in">
-              <h2>
-                POWER
-              </h2>
-              <p>MULTIPLE DEVICES</p>
-              <p>UNLIMITED CONCURRENT SESSIONS</p>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section>
-
-    <!-- ======= Partner  Section ======= -->
-    <?php include "../../subPartnersSlider.php"; ?>
-
-  </main>
-
-
-  <!-- Footer Section from the main directory  -->
-  <?php include "../../footer.php" ?>
-  <!--End Footer Section -->
-
-  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="../../assets/vendor/jquery/jquery.min.js"></script>
-  <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="../../assets/vendor/php-email-form/validate.js"></script>
-  <script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="../../assets/vendor/venobox/venobox.min.js"></script>
-  <script src="../../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="../../assets/vendor/aos/aos.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="../../assets/js/pandasecurity-main.js"></script>
-</body>
-
+<!DOCTYPE html>
+
+
+
+<html lang="en">
+
+
+
+<head>
+
+
+
+  <meta charset="utf-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+  <meta name="Author" content="Tiger Consulting and Software Devt Inc.">
+
+  <!--<meta name="robots" content="index,follow">-->
+
+  <meta name="Description" content="Tiger Consulting is a CERTIFIED partner and reseller of anydesk.">
+
+  <meta name="keywords" content="anydesk, anydesk software, tcsdi anydesk software, tiger anydesk software, tiger consulting anydesk software, anydesk certified partner, certified partner anydesk, certified partner of anydesk, authorized reseller anydesk, authorized reseller of anydesk, authorized re-seller anydesk, authorized re-seller of anydesk, adobe authorized reseller, anydesk authorized re-seller">
+
+  <title>Products - Anydesk | Global Tiger Solutions</title>
+
+
+
+  <!-- Favicons -->
+
+  <link rel="icon" href="../../assets/img/tcsdi_icon.ico">
+
+  <link href="../../assets/img/GTSLogo.png" rel="apple-touch-icon">
+
+
+
+  <!-- Google Fonts -->
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;300;400&family=Open+Sans&display=swap" rel="stylesheet">
+
+
+
+  <!-- Vendor CSS Files -->
+
+  <link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="../../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+
+  <link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+
+  <link href="../../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+
+  <link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+
+  <link href="../../assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+
+  <link href="../../assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <link href="../../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+  <link href="../../assets/vendor/aos/aos.css" rel="stylesheet">
+
+
+
+  <!-- Template Main CSS File -->
+
+  <link href="../../assets/css/products-styles/partners.css" rel="stylesheet">
+
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+  <!--[if lt IE 9]>
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js "></script>
+
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js "></script>
+
+<![endif]-->
+
+  <script>
+    (function(i, s, o, g, r, a, m) {
+
+      i['GoogleAnalyticsObject'] = r;
+
+      i[r] = i[r] || function() {
+
+        (i[r].q = i[r].q || []).push(arguments)
+
+      }, i[r].l = 1 * new Date();
+
+      a = s.createElement(o),
+
+        m = s.getElementsByTagName(o)[0];
+
+      a.async = 1;
+
+      a.src = g;
+
+      m.parentNode.insertBefore(a, m)
+
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-64201982-1', 'auto');
+
+    ga('send', 'pageview');
+  </script>
+
+
+
+  <style>
+
+    .katawan {
+      font-family: 'Noto Sans', sans-serif;
+    }
+    .anydesk-intro {
+
+      background: linear-gradient(to right, rgba(114, 28, 106, 1) 0, rgba(204, 20, 123, 1) 100%);
+
+      color: white;
+
+    }
+
+
+
+    /* .anydesk-info {
+
+      padding-left: 40px; 
+
+    } 
+
+    */
+
+
+
+    /* Fonts */
+
+    .ph {
+
+      font-size: 56px;
+
+      line-height: 58px;
+
+    }
+
+
+
+    .font-italic {
+
+      font-style: italic
+    }
+
+
+
+    .font-extrabold {
+
+      font-weight: 800;
+
+    }
+
+
+
+    .font-underline {
+
+      text-decoration: underline;
+
+    }
+
+
+
+    .label-one {
+
+      padding-top: 3px;
+
+      font-size: 42px;
+
+      line-height: 46px;
+
+    }
+
+
+
+    .label-two {
+
+      font-size: 26px;
+
+      line-height: 32px;
+
+    }
+
+
+
+    .contactDetails {
+
+      font-size: 26px;
+
+      line-height: 32px;
+
+    }
+
+
+
+    /* Spacer */
+
+    .spacer-16 {
+
+      height: 16px;
+
+      width: 100%;
+
+    }
+
+
+
+    /* Lines */
+
+    ul.horizontal-lines>li {
+
+      padding-top: 9px !important;
+
+      padding-bottom: 9px !important;
+
+      border-bottom: solid #dedede 1px;
+
+    }
+
+
+
+    @media only screen and (max-width: 600px) {
+
+      .anydesk-info {
+
+        padding-left: 15px;
+
+        font-size: xx-small;
+
+      }
+
+
+
+      .contactDetails {
+
+        font-size: 20px;
+
+        line-height: 22px;
+
+      }
+
+
+
+    }
+  </style>
+
+
+
+</head>
+
+
+
+<body>
+
+
+
+  <!-- ======= Header ======= -->
+
+  <header id="header" class="fixed-top d-flex align-items-center  header-transparent header-scrolled">
+
+    <div class="container d-flex align-items-center">
+
+
+
+      <div class="logo mr-auto">
+
+        <h1 class="text-light"><a href="../../index.php"><img src="../../assets/img/gtslogo.png" alt="" class="img-fluid"></a></h1>
+
+        <!-- Uncomment below if you prefer to use an image logo -->
+
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
+
+      </div>
+
+
+
+      <!-- ======= Nav Menu Section ======= -->
+
+      <?php include "../../navMenu.php"; ?>
+
+
+
+
+
+    </div>
+
+  </header>
+
+  <!-- End Header -->
+
+
+
+  <main id="main">
+
+
+
+
+    <section id="partner" class="partner" style="padding-top: 100px !important;">
+
+
+
+      <div class="container">
+
+
+
+        <div class="section-title" data-aos="zoom-out">
+
+          <p>Partners</p>
+
+          <h2>Anydesk</h2>
+
+        </div>
+
+        <!-- <div class="bg-color-gray-sm bg-img-none-sm bg-pos-y-0 darkmode container-hero license-configurator-monitor-jpg-hero-img" data-darkmode="license-configurator-monitor-jpg-hero-img-darkmode">
+    <div class="container-fluid">
+        <div class="row  text-left">
+        <div class="col-sm-8 col-md-7 col-lg-6 offset-sm-4 offset-md-5 offset-lg-6 text-center text-sm-left">
+            <div class="divider d-none d-sm-block"></div>
+            <div class="divider-sm d-sm-none"></div>
+            <h3 class="font-color mb-1 mt-2">Unsure which license<br>fits your needs?</h3>
+            <p class="h5 bold red mb-0">We guide you through our plans in less than a minute.</p>
+            <a class="btn btn-primary mt-2 mr-1" href="/en/license-configurator">
+	<p class="mb-0 d-inline-block">Go to the License Configurator</p>
+    </a><div class="divider-sm"></div>
+        </div></div>
+        </div>
+</div> -->
+
+        <!-- New Version 
+
+        <div class="partners-img">
+
+              <img src="../../assets/img/productpagebanner/anydesk/anydesk-norton.jpg" alt="BG Anvitirus" width="578px" height="370px">
+
+        </div>
+
+        -->
+
+
+
+      </div>
+
+
+
+
+
+    </section>
+
+
+
+
+
+    <section id="anydesk-discover" class="pricing text-center my-2" style="padding-top:10px !important">
+
+      <div class="container">
+
+        <img src="../../assets/img/productpagebanner/anydesk/anydesk1.png" alt="Discover Anydesk" class="img-fluid">
+
+        <img src="../../assets/img/productpagebanner/anydesk/anydesk2.png" alt="Remote Support" class="img-fluid">
+
+        <img src="../../assets/img/productpagebanner/anydesk/anydesk4.png" alt="Work From Home" class="img-fluid">
+
+        <img src="../../assets/img/productpagebanner/anydesk/anydesk3.png" alt="Performance | Security" class="img-fluid">
+
+        
+
+      </div>
+
+    </section>
+
+
+
+
+
+    <section id="anydesk-feature" class="pricing text-center" style="padding-top:10px !important">
+
+
+
+      <div id="anydesk-feature" class="section-sub-title text-center" data-aos="zoom-out">
+
+        <h2>Solutions</h2>
+
+      </div>
+
+
+
+      <div class="container">
+
+        <div class="row pt-4">
+
+
+
+          <div class="col-lg-4 col-md-6">
+
+            <div class="box featured text-center" data-aos="zoom-in">
+
+              <h2>
+
+                SOLO
+
+              </h2>
+
+              <p>ONE DEVICE</p>
+
+              <p>1 SESSION</p>
+
+            </div>
+
+          </div>
+
+
+
+          <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+
+            <div class="box featured text-center" data-aos="zoom-in">
+
+              <h2>
+
+                STANDARD
+
+              </h2>
+
+              <p>MULTIPLE DEVICES</p>
+
+              <p>1 OR MORE CONCURRENT SESSIONS</p>
+
+            </div>
+
+          </div>
+
+
+
+          <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+
+            <div class="box featured text-center" data-aos="zoom-in">
+
+              <h2>
+
+                ADVANCED
+
+              </h2>
+
+              <p>MULTIPLE DEVICES</p>
+
+              <p>UNLIMITED CONCURRENT SESSIONS</p>
+
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+
+
+      </div>
+
+    </section>
+
+
+
+    <!-- ======= Partner  Section ======= -->
+
+
+
+
+  </main>
+
+
+
+
+
+  <!-- Footer Section from the main directory  -->
+
+  <?php include "../../footer.php" ?>
+
+  <!--End Footer Section -->
+
+
+
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+
+
+
+  <!-- Vendor JS Files -->
+
+  <script src="../../assets/vendor/jquery/jquery.min.js"></script>
+
+  <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <script src="../../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+
+  <script src="../../assets/vendor/php-email-form/validate.js"></script>
+
+  <script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+  <script src="../../assets/vendor/venobox/venobox.min.js"></script>
+
+  <script src="../../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+
+  <script src="../../assets/vendor/aos/aos.js"></script>
+
+
+
+  <!-- Template Main JS File -->
+
+  <script src="../../assets/js/pandasecurity-main.js"></script>
+
+</body>
+
+
+
 </html>
