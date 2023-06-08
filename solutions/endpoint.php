60dfb3fe4f87bdea058bb325a83b58c4e17caf19
yrcasa <yvanroi.casa2023@gmail.com>
2023-06-08 16:58:19 +0800

    added Norton and Panda Security

--- solutions/endpoint.php
+++ solutions/endpoint.php
@@ -18,7 +18,7 @@
 			<div class="logo mr-auto">
 				<h1 class="text-light">
 					<!-- <a href="index.html">GTS</a> -->
-					<a href="index.php"><img src="../assets/img/gtslogo.png" alt="" class="img-fluid"></a>
+					<a href="../index.php"><img src="../assets/img/gts_v2.webp" alt="" class="img-fluid"></a>
 				</h1>
 				<!-- Uncomment below if you prefer to use an image logo -->
 			</div>
@@ -26,19 +26,37 @@
 		</div>
 	</header><!-- End Header -->
 	<main id="main">
-		<section class="partner" style="padding-top: 100px !important;">
+		<section class="partner" style="padding-top: 100px !important; padding-bottom: 100px !important;">
 			<div class="container">
 				<div class="section-title" data-aos="zoom-out">
 					<p>Solutions</p>
 					<h2>Endpoint Protection and Security</h2>
 				</div>
 				<div class="row content">
-					<div class="col-lg-4 col-md-8 col-sm-12 center-block">
-						<img src="../assets/img/solutions/heimdal.png" alt="" srcset="" width="100%">
+					<div class="col-lg-4 col-md-8 col-sm-12 center-block mt-4">
+						<img src="../assets/img/solutions/heimdal1.png" alt="" srcset="" width="100%">
 					</div>
 					<div class="col-md-8 col-sm-16 mt-3">
 						<h1>Heimdal Security</h1>
 						<p> Heimdal Security is a leading cybersecurity company that protects individuals and businesses from online threats. With advanced solutions like Thor, we detect and block malware, ransomware, and phishing attacks in real-time. Our proactive approach includes traffic filtering and patch management to ensure secure browsing and software protection. Trust Heimdal Security for comprehensive cybersecurity services and reliable digital defense. </p>
+					</div>
+				</div>
+				<div class="row content">
+					<div class="col-lg-4 col-md-8 col-sm-12 center-block mt-4">
+						<img src="../assets/img/solutions/panda.png" alt="" srcset="" width="100%">
+					</div>
+					<div class="col-md-8 col-sm-16 mt-3">
+						<h1>Panda Security</h1>
+						<p> Panda Security is a highly respected cybersecurity organization known for its state-of-the-art offerings in safeguarding individuals and businesses from online threats. Recognized for its emphasis on technological advancements, Panda Security provides comprehensive antivirus, firewall, and threat detection solutions to protect users' devices and information. With user-friendly interfaces and a cloud-based infrastructure, they deliver efficient and real-time security, establishing themselves as a reliable option in the ever-changing field of cybersecurity.. </p>
+					</div>
+				</div>
+				<div class="row content">
+					<div class="col-lg-4 col-md-8 col-sm-12 center-block mt-4">
+						<img src="../assets/img/solutions/norton.png" alt="" srcset="" width="100%">
+					</div>
+					<div class="col-md-8 col-sm-16 mt-3">
+						<h1>Norton</h1>
+						<p> Norton is a renowned cybersecurity brand, offering comprehensive software solutions to protect computers and mobile devices from online threats, including antivirus, firewall, VPN, and parental control features, with a strong reputation for reliability, user-friendliness, and trusted customer support. </p>
 					</div>
 				</div>
 				<div class="row content py-2">
@@ -55,7 +73,6 @@
 						<i class="icofont-arrow-left"></i> Other Partners </a>
 				</div>
 			</div>
-		</section> <?php include "../subPartnersSlider.php"; ?>
 	</main>
 	<!-- Footer Section  --> <?php include "../footer.php" ?> <!--End Footer Section -->
 	<a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
