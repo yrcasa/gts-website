fad60b3a8993b58fa6f068e49d01cb5bd8aa13fb
yrcasa <yvanroi.casa2023@gmail.com>
2023-06-19 16:45:08 +0800

    replace form

--- forms/index.php
+++ forms/index.php
@@ -1,82 +1,188 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="Author" content="Tiger Consulting and Software Devt Inc.">
-    <meta name="Description" content="Tiger Consulting and Software Devt Inc. provides companies with cost-effective enterprise business solutions.">
-    <!--<meta name="robots" content="index,follow">-->
-    <meta name="keywords" content="account deal registration tcsdi, account deal registration tiger, tcsdi account deal registration, tiger account deal registration, account deal registrationtiger consulting, tiger consulting account deal registration">
-
-  <title>Account Deal Registration Form | Global Tiger Solutions</title>
-
-  <!-- Favicons -->
-    <link rel="icon" href="../assets/img/tcsdi_icon.ico">
-   <link href="../assets/img/GTSLogo.png" rel="icon">
-
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="../assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
-  <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="../assets/css/expertise-style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Selecao - v2.1.0
-  * Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-   <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-64201982-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-</head>
-
-<body>
-
- 
-<script type="text/javascript" src="https://form.jotform.com/jsform/202650909929060"></script>
-  
-  <!-- Vendor JS Files -->
-  <script src="../assets/vendor/jquery/jquery.min.js"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="../assets/vendor/php-email-form/validate.js"></script>
-  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="../assets/vendor/venobox/venobox.min.js"></script>
-  <script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="../assets/vendor/aos/aos.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="../assets/js/expertise-main.js"></script>
-
-  <script type="text/javascript">
-
-    $('.carousel').carousel({
-      interval: 6000,
-      pause: "false"
-    });
-                 
-  </script>
-</body>
-
+<?php include('../loader.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="tcsdi endpoint, gts endpoint, global tiger solutions endpoint, endpoint security, endpoint protection, cybersecurity">
+  <title>Become A Partner | Global Tiger Solutions</title>
+  <?php include "../links.php" ?>
+  <link href="../assets/css/products-styles/partners.css" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/cta-style.css">
+  <!-- <link rel="stylesheet" href="../assets/css/style.css"> -->
+</head>
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center  header-transparent header-scrolled">
+    <div class="container d-flex align-items-center">
+      <div class="logo mr-auto">
+        <h1 class="text-light">
+          <!-- <a href="index.html">GTS</a> -->
+          <a href="../index.php"><img src="../assets/img/gts_v2.webp" alt="" class="img-fluid"></a>
+        </h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+      </div>
+      <!-- ======= Nav Menu Section ======= --> <?php include('../navMenu.php'); ?>
+    </div>
+  </header><!-- End Header -->
+  <main id="main">
+    <section class="partner" style="padding-top: 50px !important;">
+      <!-- <div class="container">
+        <div class="section-title" data-aos="zoom-out">
+          <p>Become A Partner</p>
+          <h2>Registration Form</h2>
+        </div>
+        <div class="mb-3">
+          <h2 class="h5">As a Global Tiger Solutions reseller partner, you will have access to the world’s leading technologies – and more. Our reseller partners outperform the competition and deliver next-level solutions for clients and customers thanks to our unique, value-added services.</h2>
+        </div>
+        <form action="">
+          <div class="form-group form-row">
+            <div class="col">
+              <input type="text" name="First Name" id="firstName" class="form-control" placeholder="First Name">
+            </div>
+            <div class="col">
+              <input type="text" name="Last Name" id="lastName" class="form-control" placeholder="Last Name">
+            </div>
+          </div>
+          <div class="form-group">
+            <div>
+              <input type="email" name="" id="email" class="form-control" placeholder="Email">
+            </div>
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Company Name" class="form-control" id="companyName">
+          </div>
+          <div class="form-group">
+            <input type="tel" name="" id="contactNumber" placeholder="Contact Number" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Position" class="form-control" id="position">
+          </div>
+          <div class="text">
+            <input type="button" value="Submit" class="btn btn-primary">
+          </div>
+        </form>
+      </div> -->
+
+      <iframe id="JotFormIFrame-231648922077058" title="Become A Partner" onload="window.parent.scrollTo(0,0)" allowtransparency="true" allowfullscreen="true" allow="geolocation; microphone; camera" src="https://form.jotform.com/231648922077058" frameborder="0" style="min-width:100%;max-width:100%;height:539px;border:none;" scrolling="no">
+      </iframe>
+      <script type="text/javascript">
+        var ifr = document.getElementById("JotFormIFrame-231648922077058");
+        if (ifr) {
+          var src = ifr.src;
+          var iframeParams = [];
+          if (window.location.href && window.location.href.indexOf("?") > -1) {
+            iframeParams = iframeParams.concat(window.location.href.substr(window.location.href.indexOf("?") + 1).split('&'));
+          }
+          if (src && src.indexOf("?") > -1) {
+            iframeParams = iframeParams.concat(src.substr(src.indexOf("?") + 1).split("&"));
+            src = src.substr(0, src.indexOf("?"))
+          }
+          iframeParams.push("isIframeEmbed=1");
+          ifr.src = src + "?" + iframeParams.join('&');
+        }
+        window.handleIFrameMessage = function(e) {
+          if (typeof e.data === 'object') {
+            return;
+          }
+          var args = e.data.split(":");
+          if (args.length > 2) {
+            iframe = document.getElementById("JotFormIFrame-" + args[(args.length - 1)]);
+          } else {
+            iframe = document.getElementById("JotFormIFrame");
+          }
+          if (!iframe) {
+            return;
+          }
+          switch (args[0]) {
+            case "scrollIntoView":
+              iframe.scrollIntoView();
+              break;
+            case "setHeight":
+              iframe.style.height = args[1] + "px";
+              if (!isNaN(args[1]) && parseInt(iframe.style.minHeight) > parseInt(args[1])) {
+                iframe.style.minHeight = args[1] + "px";
+              }
+              break;
+            case "collapseErrorPage":
+              if (iframe.clientHeight > window.innerHeight) {
+                iframe.style.height = window.innerHeight + "px";
+              }
+              break;
+            case "reloadPage":
+              window.location.reload();
+              break;
+            case "loadScript":
+              if (!window.isPermitted(e.origin, ['jotform.com', 'jotform.pro'])) {
+                break;
+              }
+              var src = args[1];
+              if (args.length > 3) {
+                src = args[1] + ':' + args[2];
+              }
+              var script = document.createElement('script');
+              script.src = src;
+              script.type = 'text/javascript';
+              document.body.appendChild(script);
+              break;
+            case "exitFullscreen":
+              if (window.document.exitFullscreen) window.document.exitFullscreen();
+              else if (window.document.mozCancelFullScreen) window.document.mozCancelFullScreen();
+              else if (window.document.mozCancelFullscreen) window.document.mozCancelFullScreen();
+              else if (window.document.webkitExitFullscreen) window.document.webkitExitFullscreen();
+              else if (window.document.msExitFullscreen) window.document.msExitFullscreen();
+              break;
+          }
+          var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
+          if (isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
+            var urls = {
+              "docurl": encodeURIComponent(document.URL),
+              "referrer": encodeURIComponent(document.referrer)
+            };
+            iframe.contentWindow.postMessage(JSON.stringify({
+              "type": "urls",
+              "value": urls
+            }), "*");
+          }
+        };
+        window.isPermitted = function(originUrl, whitelisted_domains) {
+          var url = document.createElement('a');
+          url.href = originUrl;
+          var hostname = url.hostname;
+          var result = false;
+          if (typeof hostname !== 'undefined') {
+            whitelisted_domains.forEach(function(element) {
+              if (hostname.slice((-1 * element.length - 1)) === '.'.concat(element) || hostname === element) {
+                result = true;
+              }
+            });
+            return result;
+          }
+        };
+        if (window.addEventListener) {
+          window.addEventListener("message", handleIFrameMessage, false);
+        } else if (window.attachEvent) {
+          window.attachEvent("onmessage", handleIFrameMessage);
+        }
+      </script>
+    </section>
+  </main>
+  <!-- Footer Section  --> <?php include "../footer.php" ?> <!--End Footer Section -->
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+  <!-- Vendor JS Files -->
+  <script src="../../assets/vendor/jquery/jquery.min.js"></script>
+  <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="../../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../../assets/vendor/venobox/venobox.min.js"></script>
+  <script src="../../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="../../assets/vendor/aos/aos.js"></script>
+  <!-- Template Main JS File -->
+  <script src="../../assets/js/pandasecurity-main.js"></script>
+</body>
+
 </html>
