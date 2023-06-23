1eae6fd79a88b6638c9d054cb595fc76d2c3e423
yrcasa <yvanroi.casa2023@gmail.com>
2023-06-23 16:09:06 +0800

    initial commit

---
+++ ourstory.php
@@ -0,0 +1,195 @@
+<!--
+    Initial commit by Yvan
+    Edited by Yvan
+-->
+
+<!DOCTYPE html>
+<?php include('loader.php') ?>
+
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#ef6603">
+    <meta property="og:title" content="Global Tiger Solutions">
+    <!-- open Graph for Sharing Posts through Social Media -->
+    <meta property="og:site_name" content="Global Tiger Solutions | Cybersecurity and IT Distributor">
+    <meta property="og:url" content="https://globaltigersolutions.com/">
+    <meta property="og:description" content="Founded in 2008, Global Tiger Solutions distributes sustainable systems, solutions, and services for a fast-paced community, emphasizing innovation, integration, and a future-present approach.">
+    <meta property="og:type" content="business.business">
+    <meta property="og:image" content="https://globaltigersolutions.com/assets/img/gtslogo.png">
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="globaltigersolutions.com">
+    <meta property="twitter:url" content="https://globaltigersolutions.com">
+    <meta name="twitter:title" content="Global Tiger Solutions | Cybersecurity and IT Distributor">
+    <meta name="twitter:description" content="Founded in 2008, Global Tiger Solutions distributes sustainable systems, solutions, and services for a fast-paced community, emphasizing innovation, integration, and a future-present approach.">
+    <meta name="twitter:image" content="https://globaltigersolutions.com/assets/img/gtslogo.png">
+
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="Author" content="Global Tiger Solutions">
+    <meta name="Description" content="Global Tiger Solutions provides companies with cost-effective enterprise business solutions.">
+    <meta name="keywords" content="tcsdi, global tiger solutions, internet security, antivirus distributor, endpoint protection, flight information display, fids, foxit, anydesk, bullguard, heimdal, Cybersecurity">
+    <!-- Keywords Used: 2,3,4,5,12,13,14,17,23,24 -->
+    <meta name="robots" content="Index, Follow">
+    <meta name="revisit-after" content="7 days">
+    <title>Our Story | Global Tiger Solutions</title>
+    <!-- Favicons -->
+    <link rel="icon" href="assets/img/tcsdi_icon.ico">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="https://api.fontshare.com/v2/css?f[]=general-sans@500,600,400&display=swap" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> -->
+    <!-- Template Main CSS File -->
+    <link rel="stylesheet" href="assets/css/story-style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="../assets/css/products-styles/partners.css" rel="stylesheet">
+    <!-- =======================================================
+
+
+
+  * Template Name: Selecao - v2.1.0
+
+
+
+  * Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
+
+
+
+  * Author: BootstrapMade.com
+
+
+
+  * License: https://bootstrapmade.com/license/
+
+
+
+  ======================================================== -->
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9144299-1"></script>
+    <script async>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-9144299-1');
+        /* Custom Search Element API */
+        (function() {
+            var cx = '004381368140357569196:v38j-dz2r1k';
+            var gcse = document.createElement('script');
+            gcse.type = 'text/javascript';
+            gcse.async = true;
+            gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(gcse, s);
+        })();
+    </script>
+</head>
+
+<body>
+    <header id="header" class="fixed-top d-flex align-items-center">
+        <?php include "navMenu.php" ?>
+    </header>
+    <main id="main">
+        <section id="story" class="story" style="padding-top: 100px !important; padding-bottom: 100px !important;">
+            <div class="container">
+                <div class="section-title" data-aos="zoom-out">
+                    <p>Our Story</p>
+                    <h2>Who We Are</h2>
+                </div>
+                <div class="row content py-2">
+                    <div class="col-md-5 col-sm-12 center-block my-4">
+                        <img src="assets/img/1.png" alt="" srcset="" width="100%" class="justify-content-center" data-aos="zoom-out">
+                    </div>
+                    <div class="col-md-7 col-sm-16 mt-4">
+                        <h4 class="story-description" data-aos="fade-right"><strong>Global Tiger Solutions</strong> was established in 2008 to focus mainly on systems integration and implementation of enterprise utilities solutions. It wasn't until 2018 that the company's business model was subsequently changed to IT Distribution.</h4>
+                        <h4 class="story-description" data-aos="fade-left">Our solutions focus on the cybersecurity strategy of enterprises namely <strong>Threat Prevention</strong>, <strong>Endpoint Detection, Protection and Response (EPDR)</strong>, <strong>Privileges and Application Control (PAM)</strong>, and <strong>Threat Hunting</strong>.</h4>
+                    </div>
+                </div>
+                <div class="row content my-4 justify-content-center">
+                    <h1 data-aos="fade-up">Over the years, Global Tiger Solutions has developed partnerships</h1>
+                    <div class="row row-center-mobile">
+                        <div class="col-md-3">
+                            <img src="assets/img/solutions/watchguard.webp" alt="Watchguard" data-aos="fade-up">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="assets/img/solutions/panda.webp" alt="Panda Security" data-aos="fade-up" data-aos-delay="100">
+                        </div>
+                        <div class="col-md-3 mt-3">
+                            <img src="assets/img/solutions/heimdal1.webp" alt="Heimdal" data-aos="fade-up" data-aos-delay="200" width="90%">
+                        </div>
+                        <div class="col-md-3 mt-3">
+                            <img src="assets/img/solutions/norton.webp" alt="Norton" data-aos="fade-up" data-aos-delay="300" width="90%">
+                        </div>
+                    </div>
+                    <div class="row mx-3 justify-content-center my-3">
+                        <div class="col-md-2 img-mobile">
+                            <img src="assets/img/solutions/netgate.webp" alt="Netgate" data-aos="fade-up" width="120%">
+                        </div>
+                        <div class="col-md-2 ml-4 mt-2">
+                            <img src="assets/img/solutions/agisoft_v2.png" alt="Panda Security" data-aos="fade-up" data-aos-delay="100" width="100%">
+                        </div>
+                        <div class="col-md-2 mt-3">
+                            <img src="assets/img/solutions/anydesk.png" alt="Heimdal" data-aos="fade-up" data-aos-delay="200" width="100%">
+                        </div>
+                        <div class="col-md-2">
+                            <img src="assets/img/solutions/foxit-logo.webp" alt="Norton" data-aos="fade-up" data-aos-delay="300" width="90%">
+                        </div>
+                        <div class="col-md-2 mt-2">
+                            <img src="assets/img/solutions/navicat_logo_rainbow.png" alt="Norton" data-aos="fade-up" data-aos-delay="400" width="100%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row content">
+                    <div class="col-md-6 my-2 py-2">
+                        <h4 class="story-description h4 text-justify" data-aos="zoom-out">Global Tiger Solutions' vision is to a be leading provider of sustainable systems, solutions and services that are tailor-fitted for a fast-paced community where innovation and integration is the backbone towards a progressive and sound management, where the future can be the present.</h4>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="assets/img/5.png" alt="" data-aos="zoom-out" width="100%">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <?php include "footer.php" ?>
+    </footer>
+    <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <!-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <script type="text/javascript">
+        $('.carousel').carousel({
+            interval: 6000,
+            pause: "false"
+        });
+    </script>
+</body>
