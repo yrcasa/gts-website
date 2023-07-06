3b735a5e5775789c31d8c8c239e26722e6284a7a
yrcasa <yvanroi.casa2023@gmail.com>
2023-07-06 15:56:48 +0800

    recreate cta, removing image

--- cta.php
+++ cta.php
@@ -2,24 +2,17 @@
   <div class="container">
     <div class="row row-cols-1">
       <h3 class="text-center" data-aos="zoom-out">OUR SOLUTIONS</h3>
-      <div class="row row-cols-1 mx-3 row-cols-md-1 my-2 py-2 row-cols-sm-1" data-aos="fadeUp">
-        <div class="col d-flex justify-content-center mb-3" data-aos="fade-right">
+      <div class="row row-cols-1 row-cols-md-2 my-2 py-2 row-cols-sm-1">
+        <div class="col mb-3" data-aos="fade-right">
           <div class="card">
             <div class="row g-0">
-              <div class="col-md-7">
-                <div class="image-container">
-                  <img alt=... class="img-card" src="assets\img\cta\endpoint_v2.webp">
-                  <div class="overlay"></div>
-                </div>
-              </div>
-              <div class="col-md-5">
+              <div class="col">
                 <div class="card-body">
                   <a href="solutions/endpoint.php">
+                    <img src="assets/img/cta/Endpoint_icon_color.png" alt="" width="25%" class="mb-2">
                     <h4 class="card-title text-muted">Endpoint Protection and Security</h4>
                     <p class="card-text text-muted">Comprehensive defense for devices and networks, safeguarding against cyber threats and ensuring data integrity.</p>
-                    <div class="mt-5">
-                      <center><img src="assets/img/cta/Endpoint_icon_color.png" alt="" width="50%"></center>
-                    </div>
+                    <a href="solutions/endpoint.php" class="btn btn-primary cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
                   </a>
                 </div>
               </div>
@@ -27,125 +20,88 @@
           </div>
         </div>
         <!-- Add more columns with the same structure for other cards -->
-        <div class="col d-flex justify-content-center mb-3" data-aos="fade-left">
+        <div class="col mb-3" data-aos="fade-left">
           <div class="card">
             <div class="row g-0">
-              <div class="col-md-5">
+              <div class="col">
                 <div class="card-body">
                   <a href="solutions/networkSecurity.php">
+                    <img src="assets/img/cta/EPP_Icon_Color.png" alt="" width="25%" class="mb-2">
                     <h4 class="card-title text-muted">Network Security and Firewall</h4>
                     <p class="card-text text-muted">Essential defense system that monitors and controls network traffic, preventing unauthorized access and protecting against cyber attacks.</p>
-                    <div class="mt-5">
-                      <center><img src="assets/img/cta/EPP_Icon_Color.png" alt="" width="40%"></center>
-                    </div>
+                    <a href="solutions/networkSecurity.php" class="btn btn-primary cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
                   </a>
-                </div>
-              </div>
-              <div class="col-md-7">
-                <div class="image-container">
-                  <img alt=... class="img-card" src="assets\img\cta\networkSecurity.jpg">
-                  <div class="overlay"></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col d-flex justify-content-center mb-3" data-aos="fade-right">
+        <!-- Add more columns with the same structure for other cards -->
+        <div class="col mb-3" data-aos="fade-right">
           <div class="card">
             <div class="row g-0">
-              <div class="col-md-7">
-                <div class="image-container">
-                  <img alt=... class="img-card" src="assets\img\cta\privilegedAccess.webp">
-                  <div class="overlay"></div>
-                </div>
-              </div>
-              <div class="col-md-5">
+              <div class="col">
                 <div class="card-body">
                   <a href="solutions/privilegedAccess.php">
+                    <img src="assets/img/cta/EDR_Icon_Color.png" alt="" width="25%" class="mb-2">
                     <h4 class="card-title text-muted">Privileges and Application Control</h4>
                     <p class="card-text text-muted">Secure solution managing privileged accounts, minimizing insider threats and ensuring authorized access.</p>
-                    <div class="mt-5">
-                      <center><img src="assets/img/cta/EDR_icon_color.png" alt="" width="50%"></center>
-                    </div>
+                    <a href="solutions/privilegedAccess.php" class="btn btn-primary cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
                   </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col d-flex justify-content-center mb-3" data-aos="fade-left">
+        <div class="col mb-3" data-aos="fade-left">
           <div class="card">
             <div class="row g-0">
-              <div class="col-md-5">
+              <div class="col">
                 <div class="card-body">
                   <a href="solutions/remote.php">
+                    <img src="assets/img/cta/Laptop_Endpoint_Security_Shield (1).png" alt="" width="40%" class="mb-2">
                     <h4 class="card-title text-muted">Remote Access and Monitoring Security</h4>
                     <p class="card-text text-muted">Convenient and secure solution that enables remote connectivity and real-time monitoring of systems and networks from any location.</p>
-                    <div class="mt-5">
-                      <center><img src="assets/img/cta/Laptop_Endpoint_Security_Shield (1).png" alt="" width="60%"></center>
-                    </div>
+                    <a href="solutions/remote.php" class="btn btn-primary cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
                   </a>
-                </div>
-              </div>
-              <div class="col-md-7">
-                <div class="image-container">
-                  <img alt=... class="img-card" src="assets\img\cta\remote.webp">
-                  <div class="overlay"></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col d-flex justify-content-center mb-3" data-aos="fade-right">
+        <div class="col mb-3" data-aos="fade-right">
           <div class="card">
             <div class="row g-0">
-              <div class="col-md-7">
-                <div class="image-container">
-                  <img alt=... class="img-card" src="assets/img/carousel/fids-new-banner.png">
-                  <div class="overlay"></div>
-                </div>
-              </div>
-              <div class="col-md-5">
+              <div class="col">
                 <div class="card-body">
                   <a href="solutions/aviation.php">
-                    <h4 class="card-title text-muted">Aviation Solutions</h4>
+                    <i class="icofont-airplane-alt icofont-6x text-muted"></i>
+                    <h4 class="card-title text-muted mt-4">Aviation Solutions</h4>
                     <p class="card-text text-muted">Real-time solution displaying flight information, gate assignments, and schedules for seamless passenger experience and operational efficiency.</p>
-                    <div class="mt-5">
-                      <center><i class="icofont-airplane-alt icofont-10x text-muted"></i></center>
-                    </div>
+                    <a href="solutions/aviation.php" class="btn btn-primary cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
                   </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col d-flex justify-content-center mb-3" data-aos="fade-left">
+        <div class="col mb-3" data-aos="fade-left">
           <div class="card">
             <div class="row g-0">
-              <div class="col-md-5">
+              <div class="col">
                 <div class="card-body">
                   <a href="solutions/productivity.php">
-                    <h4 class="card-title text-muted">Productivity Solutions</h4>
+                    <i class="icofont-file-alt icofont-6x text-muted"></i>
+                    <h4 class="card-title text-muted mt-4">Productivity Solutions</h4>
                     <p class="card-text text-muted">Powerful tools for editing, creating, and managing PDF documents, streamlining workflows and enhancing collaboration across platforms.</p>
-                    <div class="mt-5">
-                      <center><i class="icofont-file-alt icofont-10x text-muted"></i></center>
-                    </div>
+                    <a href="solutions/productivity.php" class="btn btn-primary cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
                   </a>
-                </div>
-              </div>
-              <div class="col-md-7">
-                <div class="image-container">
-                  <img alt=... class="img-card" src="assets\img\cta\productivity.webp">
-                  <div class="overlay"></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="text-center col-lg-12 cta-btn-container justify-content-center">
-      <a href="partner.php" class="btn btn-primary align-middle cta-btn">Learn More</a>
     </div>
   </div>
 </section>
