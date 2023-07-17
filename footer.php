bb28a57c1ff1ad3f2279d851b23a0953c2a71e32
yrcasa <yvanroi.casa2023@gmail.com>
2023-07-17 16:36:57 +0800

    icon color

--- footer.php
+++ footer.php
@@ -44,10 +44,10 @@
         <div class="" id="">
             <h3>Contact Us</h3>
             <ul class="nav-flex-column list-unstyled">
-                <li class="list-item mb-0 mx-1"><i class="icofont-location-pin" style="font-size: 15px;"></i><a href="https://www.google.com/maps/place/Global+Tiger+Solutions/@14.562128,121.0405898,17z/data=!3m1!4b1!4m6!3m5!1s0x3397c91b100c8323:0xf9f0f08bfc5634bf!8m2!3d14.562128!4d121.0427785!16s%2Fg%2F11qpx18n2l"> 3rd Floor Pet Plans Tower, 444 EDSA Guadaupe Viejo, Makati City, Philippines 1211</a></li>
+                <li class="list-item mb-0 mx-1"><i class="icofont-location-pin" style="font-size: 15px; color: #fff;"></i><a href="https://www.google.com/maps/place/Global+Tiger+Solutions/@14.562128,121.0405898,17z/data=!3m1!4b1!4m6!3m5!1s0x3397c91b100c8323:0xf9f0f08bfc5634bf!8m2!3d14.562128!4d121.0427785!16s%2Fg%2F11qpx18n2l"> 3rd Floor Pet Plans Tower, 444 EDSA Guadaupe Viejo, Makati City, Philippines 1211</a></li>
                 <br>
-                <li class="list-item mb-0"><i class="icofont-phone" style="font-size: 15px;"></i><a href="tel:+63 2 8352 825"> +63 2 8352 8250</a></li>
-                <li class="list-item mb-0"><i class="icofont-envelope" style="font-size: 15px;"></i><a href="mailto:sales@globaltigersolutions.com"> sales@globaltigersolutions.com</a></li>
+                <li class="list-item mb-0"><i class="icofont-phone" style="font-size: 15px; color: #fff;"></i><a href="tel:+63 2 8352 825"> +63 2 8352 8250</a></li>
+                <li class="list-item mb-0"><i class="icofont-envelope" style="font-size: 15px; color: #fff;"></i><a href="mailto:sales@globaltigersolutions.com"> sales@globaltigersolutions.com</a></li>
             </ul>
             <ul class="nav-flex-column list-unstyled list-group list-group-horizontal d-flex justify-content-center">
                 <li class="nav-item mb-0 m-1 mt-1"><a href="https://www.facebook.com/tcsdi/"><i class="bi bi-facebook" style="font-size:20px"></i></a></li>
