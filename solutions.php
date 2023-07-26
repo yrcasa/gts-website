d7d03d51a44a234b03520cf01ad3fc28d7d64f6b
yrcasa <yvanroi.casa2023@gmail.com>
2023-07-26 17:01:01 +0800

    initial commit

---
+++ solutions.php
@@ -0,0 +1,238 @@
+<!--
+    Initial commit by Yvan
+    Edited by Yvan
+-->
+
+<!DOCTYPE html>
+<?php include('loader.php') ?>
+
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#ef6603">
+    <meta property="og:title" content="Global Tiger Solutions">
+    <!-- open Graph for Sharing Posts through Social Media -->
+    <meta property="og:site_name" content="Global Tiger Solutions | Cybersecurity and IT Distributor">
+    <meta property="og:url" content="https://globaltigersolutions.com/">
+    <meta property="og:description" content="Founded in 2008, Global Tiger Solutions distributes sustainable systems, solutions, and services for a fast-paced community, emphasizing innovation, integration, and a future-present approach.">
+    <meta property="og:type" content="business.business">
+    <meta property="og:image" content="https://globaltigersolutions.com/assets/img/gtslogo.png">
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="globaltigersolutions.com">
+    <meta property="twitter:url" content="https://globaltigersolutions.com">
+    <meta name="twitter:title" content="Global Tiger Solutions | Cybersecurity and IT Distributor">
+    <meta name="twitter:description" content="Founded in 2008, Global Tiger Solutions distributes sustainable systems, solutions, and services for a fast-paced community, emphasizing innovation, integration, and a future-present approach.">
+    <meta name="twitter:image" content="https://globaltigersolutions.com/assets/img/gtslogo.png">
+
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="Author" content="Global Tiger Solutions">
+    <meta name="Description" content="Global Tiger Solutions provides companies with cost-effective enterprise business solutions.">
+    <meta name="keywords" content="tcsdi, global tiger solutions, internet security, antivirus distributor, endpoint protection, flight information display, fids, foxit, anydesk, bullguard, heimdal, Cybersecurity">
+    <!-- Keywords Used: 2,3,4,5,12,13,14,17,23,24 -->
+    <meta name="robots" content="Index, Follow">
+    <meta name="revisit-after" content="7 days">
+    <title>Solutions | Global Tiger Solutions</title>
+    <!-- Favicons -->
+    <link rel="icon" href="assets/img/tcsdi_icon.ico">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="https://api.fontshare.com/v2/css?f[]=general-sans@500,600,400&f[]=clash-grotesk@400,600,500&display=swap" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> -->
+    <!-- Template Main CSS File -->
+    <link rel="stylesheet" href="assets/css/story-style.css">
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+    <link href="assets/css/products-styles/partners.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/cta-style.css">
+    <!-- <link href="../assets/css/about-style.css" rel="stylesheet"> -->
+    <!-- =======================================================
+
+
+
+  * Template Name: Selecao - v2.1.0
+
+
+
+  * Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
+
+
+
+  * Author: BootstrapMade.com
+
+
+
+  * License: https://bootstrapmade.com/license/
+
+
+
+  ======================================================== -->
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9144299-1"></script>
+    <script async>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-9144299-1');
+        /* Custom Search Element API */
+        (function() {
+            var cx = '004381368140357569196:v38j-dz2r1k';
+            var gcse = document.createElement('script');
+            gcse.type = 'text/javascript';
+            gcse.async = true;
+            gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(gcse, s);
+        })();
+    </script>
+</head>
+
+<body>
+    <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+        <?php include "navMenu.php" ?>
+    </header>
+    <main id="main">
+        <section id="hero" class="d-flex flex-column justify-content-end align-items-center">
+            <div id="heroCarousel" class="container carousel carousel-fade" data-ride="carousel">
+                <!-- Slide 1 -->
+                <div class="carousel-item active">
+                    <div class="carousel-container">
+                        <h2 class="animated fadeInDown">SOLUTIONS</span></h2>
+                        <p class="animated fadeInUp">
+                            <!-- Details Here  --> Solutions for Businesses of All Sizes
+                        </p>
+                        <a href="#story" class="btn-get-started animated fadeInUp scrollto">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Wave Effect -->
+            <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+                <defs>
+                    <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+                </defs>
+                <g class="wave1">
+                    <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+                </g>
+                <g class="wave2">
+                    <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+                </g>
+                <g class="wave3">
+                    <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+                </g>
+            </svg>
+        </section>
+        <section id="story" class="partner story" style="padding-top: 100px !important;">
+            <div class="container">
+                <!-- <div class="section-title" data-aos="zoom-out">
+                    <p>Solutions</p>
+                    <h2>Our Trusted Solutions</h2>
+                </div> -->
+                <div class="row content pt-2">
+                    <div class="col-md-4">
+                        <img src="assets/img/cta/Endpoint_icon_color.png" alt="" width="70%">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Endpoint Protection and Security</h3>
+                        <p>Comprehensive defense for devices and networks, safeguarding against cyber threats and ensuring data integrity.</p>
+                        <a href="solutions/endpoint.php" class="btn rounded btn-learn-more cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row content pt-2">
+                    <div class="col-md-4">
+                        <img src="assets/img/cta/EPP_Icon_Color.png" alt="" width="70%">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Network Security and Firewall</h3>
+                        <p>Essential defense system that monitors and controls network traffic, preventing unauthorized access and protecting against cyber attacks.</p>
+                        <a href="solutions/networkSecurity.php" class="btn rounded btn-learn-more cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row content pt-2">
+                    <div class="col-md-4">
+                        <img src="assets/img/cta/EDR_Icon_Color.png" alt="" width="70%">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Privileges and Application Control</h3>
+                        <p>Secure solution managing privileged accounts, minimizing insider threats and ensuring authorized access.</p>
+                        <a href="solutions/privilegedAccess.php" class="btn rounded btn-learn-more cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row content pt-2">
+                    <div class="col-md-4">
+                        <img src="assets/img/cta/Laptop_Endpoint_Security_Shield (1).png" alt="" width="80%">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Remote Access and Monitoring</h3>
+                        <p>Convenient and secure solution that enables remote connectivity and real-time monitoring of systems and networks from any location.</p>
+                        <a href="solutions/remote.php" class="btn rounded btn-learn-more cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row content pt-2">
+                    <div class="col-md-4">
+                        <img src="assets/img/cta/airport tower-cuate.svg" alt="" width="70%">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Aviation Solutions</h3>
+                        <p>Real-time solution displaying flight information, gate assignments, and schedules for seamless passenger experience and operational efficiency.</p>
+                        <a href="solutions/aviation.php" class="btn rounded btn-learn-more cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row content">
+                    <div class="col-md-4">
+                        <img src="assets/img/cta/Product iteration-pana.svg" alt="" width="70%">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Productivity Solutions</h3>
+                        <p>Powerful tools for editing, creating, and managing PDF documents, streamlining workflows and enhancing collaboration across platforms.</p>
+                        <a href="solutions/productivity.php" class="btn rounded btn-learn-more cta-btn mt-4">Learn More<i class="icofont-arrow-right ml-2" style="font-size: 16px;"></i></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <?php include "footer.php" ?>
+    </footer>
+    <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <!-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <script type="text/javascript">
+        $('.carousel').carousel({
+            interval: 6000,
+            pause: "false"
+        });
+    </script>
+</body>
